92f3292642bb7475721ef224f66a4e8b328c4070
Dherrero870 <dherrero870@gmail.com>
2023-11-10 14:05:26 +0100

    Update weatherdataanalysis.py

--- weatherdataanalysis.py
+++ weatherdataanalysis.py
@@ -1,11 +1,10 @@
+# Ponemos la lista de las temperaturas
 temperatures =[
-    x == [22, 24, 19, 21, 25, 23, 20] #week 1
-    y == [20, 22, 21, 23, 24, 22, 21] #week 2
-    z == [23, 21, 20, 22, 24, 25, 23] #week 3
+    x == [22, 24, 19, 21, 25, 23, 20] 
+    y == [20, 22, 21, 23, 24, 22, 21] 
+    z == [23, 21, 20, 22, 24, 25, 23] 
 ]
-def average:
-    average = total / count
-    return average
+#Definimos las clases respecto a las semanas
 class x:
     __init__(x, total, count)
         x.total= sum(x)
@@ -19,6 +18,7 @@ class z:
     __init__(z, total, count)
         z.total= sum(z)
         z.count = len(z)
+# Calculamos las medias de las diferentes semanas
 from weatherdataanalysis import x
 def average (x, total, count):
     average = x.total / x.count
@@ -34,6 +34,7 @@ def average (z, total, count):
     average = z.total / z.count
     return average
     print (f"The average in week three is:{average}")
+#Definimos los diferentes días de la semana para calcular el de máxima temperatura
 x,y,z [0] = day 1 
 x,y,z [1] = day 2
 x,y,z [2] = day 3
@@ -41,6 +42,21 @@ x,y,z [3] = day 4
 x,y,z [4] = day 5
 x,y,z [5] = day 6
 x,y,z [6] = day 7
+# Calculamos la temperatura más alta de cada semana
+def max_temperature_week (x):
+    max_temperature = max(x)
+    return max_temperature
+print (f"La máxima temperatura en la semana uno es: {max_temperature}")
+def max_temperature_week (y):
+    max_temperature = max(y)
+    return max_temperature
+print (f"La máxima temperatura en la semana dos es: {max_temperature}")
+def max_temperature_week (z):
+    max_temperature = max(z)
+    return max_temperature
+print (f"La máxima temperatura en la semana tres  es: {max_temperature}")
+
+
   
 
 
